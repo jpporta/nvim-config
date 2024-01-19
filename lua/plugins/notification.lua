ca3dff9eecf3394d1aa4d70298ccf4bf6bf02fd4
João Porta <jpedro.porta@gmail.com>
2024-01-19 08:17:19 -0300

    noice implementation

--- lua/plugins/notification.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'rcarriga/nvim-notify',
-    event = "VeryLazy",
-    config = function()
-        local notify = require('notify')
-        notify.setup({background_colour = "#000000"});
-        vim.notify = notify
-    end
-}
