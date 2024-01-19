b297fc010a1254fd2e5223569a43af6c80625d9d
João Porta <jpedro.porta@gmail.com>
2024-01-19 09:53:55 -0300

    fix: format

--- lua/plugins/vim-tmux-nav.lua
+++ lua/plugins/vim-tmux-nav.lua
@@ -1,14 +1,17 @@
 return {
-    "christoomey/vim-tmux-navigator",
-    cmd = {
-        "TmuxNavigateLeft", "TmuxNavigateDown", "TmuxNavigateUp",
-        "TmuxNavigateRight", "TmuxNavigatePrevious"
-    },
-    keys = {
-        {"<M-h>", "<cmd>TmuxNavigateLeft<cr>"},
-        {"<M-j>", "<cmd>TmuxNavigateDown<cr>"},
-        {"<M-k>", "<cmd>TmuxNavigateUp<cr>"},
-        {"<M-l>", "<cmd>TmuxNavigateRight<cr>"},
-        {"<M-\\>", "<cmd>TmuxNavigatePrevious<cr>"}
-    }
+	"christoomey/vim-tmux-navigator",
+	cmd = {
+		"TmuxNavigateLeft",
+		"TmuxNavigateDown",
+		"TmuxNavigateUp",
+		"TmuxNavigateRight",
+		"TmuxNavigatePrevious",
+	},
+	keys = {
+		{ "<M-h>", "<cmd>TmuxNavigateLeft<cr>" },
+		{ "<M-j>", "<cmd>TmuxNavigateDown<cr>" },
+		{ "<M-k>", "<cmd>TmuxNavigateUp<cr>" },
+		{ "<M-l>", "<cmd>TmuxNavigateRight<cr>" },
+		{ "<M-\\>", "<cmd>TmuxNavigatePrevious<cr>" },
+	},
 }
