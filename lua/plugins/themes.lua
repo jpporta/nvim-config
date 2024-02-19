f1574ee17aaa81d47bbecd3630834bb0490d6d8f
João Porta <jpedro.porta@gmail.com>
2024-02-19 08:04:20 -0300

    revert: transparency

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,16 +1,23 @@
 return {
-    {'rebelot/kanagawa.nvim', priority = 1000, lazy = true},
-    { -- Theme inspired by Atom
-        'navarasu/onedark.nvim',
-        priority = 1000,
-        lazy = true
-    },
-    {
-        'catppuccin/nvim',
-        lazy = true,
-        name = "catppuccin",
-        priority = 1000
-    }, {'sainnhe/everforest', lazy = true, priority = 1000},
-    {"ellisonleao/gruvbox.nvim", lazy = true, priority = 1000},
-				{'tribela/vim-transparent'}
+	{ "rebelot/kanagawa.nvim", priority = 1000, lazy = true },
+	{ -- Theme inspired by Atom
+		"navarasu/onedark.nvim",
+		priority = 1000,
+		lazy = true,
+	},
+	{
+		"catppuccin/nvim",
+		lazy = true,
+		name = "catppuccin",
+		priority = 1000,
+	},
+	{ "sainnhe/everforest", lazy = true, priority = 1000 },
+	{ "ellisonleao/gruvbox.nvim", lazy = true, priority = 1000 },
+	{
+		"xiyaowong/transparent.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("transparent").clear_prefix("lualine")
+		end,
+	},
 }
